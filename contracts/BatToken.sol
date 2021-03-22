3605925fd59b664c1256a6256a7049b80f19e9cb
Kankan-0 <kankandev@protonmail.com>
2021-03-22 17:32:41 +0530

    3.Add delegated transfer code.

--- contracts/BatToken.sol
+++ contracts/BatToken.sol
@@ -5,7 +5,9 @@ contract BatToken {
     string public name = "BAT Token";
     string public symbol = "BAT";
     uint public totalSupply;
+
     mapping(address=>uint) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(
         address indexed _from,
@@ -13,11 +15,17 @@ contract BatToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );  
+
     constructor (uint _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-    
+
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
@@ -27,4 +35,19 @@ contract BatToken {
         return true;
     }
 
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
--- test/BatTokenTest.js
+++ test/BatTokenTest.js
@@ -15,6 +15,7 @@ contract('BatToken', (accounts)=>{
             assert.equal(symbol, 'BAT', 'Has the  correct symbol');
         })
     })
+    
     it('sets the total supply upon deployment', ()=>{
         return BatToken.deployed().then(instance => {
             tokenInstance = instance;
@@ -52,6 +53,66 @@ contract('BatToken', (accounts)=>{
             return tokenInstance.balanceOf(accounts[0]);
         }).then(balance=>{
             assert.equal(balance.toNumber(), 1000000-25000, 'deducts the amount from sending account');
+        })
+    })
+
+    it('apporves tokens for delegated transfer', ()=>{
+        return BatToken.deployed().then(instance =>{
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1],100);
+        }).then(success => {
+            assert.equal(success, true,'should return true');
+            return tokenInstance.approve(accounts[1],100,{from: accounts[0]});
+        }).then(reciept=>{
+            assert.equal(reciept.logs.length, 1, 'triggers one event');
+            assert.equal(reciept.logs[0].event, 'Approval', 'should be the approve event');
+            assert.equal(reciept.logs[0].args._owner, accounts[0], 'the account from which the tokens are transferred');
+            assert.equal(reciept.logs[0].args._spender, accounts[1], 'the account to which the tokens are transferred');
+            assert.equal(reciept.logs[0].args._value, 100, 'log the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(allowance => {
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance')
+        })
+    })
+
+    it('handles delegated token transfer', ()=>{
+        return BatToken.deployed().then(instance =>{
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+
+            return tokenInstance.transfer(fromAccount,100, {from: accounts[0]});
+        }).then(reciept=>{
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount});
+        }).then(reciept => {
+            return tokenInstance.transferFrom(fromAccount, toAccount, 1000, {from: spendingAccount})    
+        }).then(assert.fail)
+        .catch(err=>{
+            assert(err.message.indexOf('revert') >= 0, 'Can not spend amount larger than balance');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount})
+        }).then(assert.fail)
+        .catch(err=>{
+            assert(err.message.indexOf('revert') >= 0, 'Can not spend amount larger than allowance');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 5, {from: spendingAccount})
+        }).then(success=>{
+            assert.equal(success, true, 'Should be true');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 5, {from: spendingAccount});
+        }).then(reciept=> {
+            assert.equal(reciept.logs.length, 1, 'triggers one event');
+            assert.equal(reciept.logs[0].event, 'Transfer', 'should be the approve event');
+            assert.equal(reciept.logs[0].args._from, fromAccount, 'the account from which the tokens are transferred');
+            assert.equal(reciept.logs[0].args._to,  toAccount, 'the account to which the tokens are transferred');
+            assert.equal(reciept.logs[0].args._value, 5, 'log the transfer amount');
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(balance => {
+            assert.equal(balance.toNumber(), 95, 'deducts from the fromAccount');
+            return tokenInstance.balanceOf(toAccount);
+        }).then(balance => {
+            assert.equal(balance.toNumber(), 5, 'adds to the toAccount');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(allowance => {
+            assert.equal(allowance.toNumber(), 5, 'remaining allowance');
         })
     })
 })
